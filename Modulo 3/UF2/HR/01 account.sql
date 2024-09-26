8906dcb3d3044ec299841dcb6bb36b47f086e4ed
Alvr965 <64633758+Alvr965@users.noreply.github.com>
2024-09-26 18:07:32 +0200

    Add files via upload

---
+++ Modulo 3/UF2/HR/01 account.sql
@@ -0,0 +1,34 @@
+/*
+01 Create Account
+Create accounts for HR schema
+Run this as either SYS or SYSTEM.
+*/
+
+
+/*
+Set the pluggable database if you're running this on Oracle v12 or later.
+*/
+ALTER SESSION SET CONTAINER = XEPDB1;
+
+/*
+If the above PDB name does not work, some options for PDB names are:
+ALTER SESSION SET CONTAINER = ORCLPDB1;
+ALTER SESSION SET CONTAINER = PDB1;
+*/
+
+/*
+Drop the HR account and all objects to reset.
+Then, create the user and give it the permissions.
+*/
+DROP USER HR CASCADE;
+
+CREATE USER HR IDENTIFIED BY HR;
+
+ALTER USER HR DEFAULT TABLESPACE users QUOTA UNLIMITED ON users;
+
+ALTER USER HR TEMPORARY TABLESPACE TEMP;
+
+GRANT CONNECT TO HR;
+
+GRANT CREATE SESSION, CREATE VIEW, CREATE TABLE, ALTER SESSION, CREATE SEQUENCE TO HR;
+GRANT CREATE SYNONYM, CREATE DATABASE LINK, RESOURCE, UNLIMITED TABLESPACE TO HR;
